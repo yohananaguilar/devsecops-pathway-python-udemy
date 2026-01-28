33edd5508359fa4ea98c32d43d31761f8dd89c76
y0shyy <yochagamer1@gmail.com>
2026-01-27 22:16:57 -0300

    Implementa exemplos de tipos primitivos e coerção de dados
    Explora os tipos de dados fundamentais do Python e typecasting:
    - aula4.py: Tipos numéricos (int e float) e uso da função type().
    - aula5.py: Tipo booleano (bool) e operadores lógicos de comparação (==).
    - aula6.py: Coerção de tipos (type convertion), convertendo valores entre str, int, float e bool.

---
+++ Lógica de Programação/aula4.py
@@ -0,0 +1,23 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número inteiro positivo ou negativo
+print(10) # int
+print(-10) # int
+print(0) # int
+print()
+
+# float -> Número com ponto flutuante
+# O tipo float representa qualquer número com ponto flutuante positivo ou negativo
+print(10.5) # float
+print(-10.5) # float
+print(0.0) # float
+print()
+
+# A função type mostra  o tipo que o Python inferiu ao valor
+print(type(10)) # int
+print(type(-10)) # int
+print(type(0)) # int
+print()
+print(type(10.5)) # float
+print(type(-10.5)) # float
+print(type(0.0)) # float
---
+++ Lógica de Programação/aula5.py
@@ -0,0 +1,10 @@
+# Tipo de dado bool (booleano)
+# Ao questionar algo em um programa, só existem duas respostas possíveis: sim(True) ou não(False)
+# Existem vários operadores para "questionar", dentr eles o ==, que é um operador lógico que questiona
+# se um valor é igual a outro
+print(10 == 10) # Sim => True(Verdadeiro)
+print(10 == 11) # Não => False(Falso)
+print(type(10 == 10)) # bool
+print(type(10 == 11)) # bool    
+print(type(True)) # bool
+print(type(False)) # bool
