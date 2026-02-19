f3f4dec616b321d399dc67af0a9b5c12d9dd9171
y0shyy <yochagamer1@gmail.com>
2026-02-18 22:27:00 -0300

    Adiciona desempacotamento e tipo tupla
    Introduz estruturas imutáveis e sintaxe avançada de atribuição:
    - Tuplas: Criação e acesso a listas imutáveis (segurança de dados).
    - Unpacking: Lógica de desempacotamento de iteráveis, utilizando '_' para ignorar valores e '*' para agrupar o restante.

---
+++ Lógica de Programação/aula52.py
@@ -0,0 +1,6 @@
+"""
+Introdução ao desempacotamento
+"""
+
+_, _, nome, *_ = ['Bil', 'Rodrigo', 'Gabriel', 'Gustavo', 'Nathan']
+print(nome)
---
+++ Lógica de Programação/aula53.py
@@ -0,0 +1,6 @@
+""" 
+Tipo tupla - Uma lista imutável
+"""
+nomes = ('Bil', 'Rodrigo', 'Gabriel', 'Gustavo', 'Nathan')
+print(nomes[0])
+print(nomes)
