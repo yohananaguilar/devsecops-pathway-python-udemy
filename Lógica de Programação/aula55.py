491a1af2188dec116d266a03328444f52c4ad75f
y0shyy <yochagamer1@gmail.com>
2026-02-23 22:29:37 -0300

    Finaliza sistema de lista de compras com tratamento de erros
    Conclui a implementação do gerenciador de listas no terminal:
    - Robustez: Adiciona blocos try/except para capturar erros de digitação (ValueError) e índices inválidos (IndexError) na remoção.
    - UX: Implementa limpeza de tela (os.system) e verificação de lista vazia.
    - Lógica: Consolida as operações de inserir, apagar e listar em um loop contínuo.

--- Lógica de Programação/aula55.py
+++ Lógica de Programação/aula55.py
@@ -3,6 +3,7 @@ Faça uma lista de comprar com listas
 O usuário deve ter a possibilidade de inserir, apagar e listar valores da sua lista 
 Não permita que o programa quebre com erros de índices inexistentes na lista.
 """
+import os
 
 lista_de_compras = []
 
@@ -10,28 +11,28 @@ while True:
     print('Selecione uma opção')
     opcoes = input('[i]nserir [a]pagar [l]istar [e]ncerrar: ')
 
-    LETRAS_PERMITIDAS = ('i', 'a', 'l', 'e')
-
-    if not LETRAS_PERMITIDAS:
-        print('Tente novamente, opção inválida!')
-        continue
-
-    elif opcoes == 'i':
+    if opcoes == 'i':
+        os.system('cls')
         valor = input('Valor: ')
         lista_de_compras.append(valor)
-        print(lista_de_compras)
-        continue
 
     elif opcoes == 'a':
-        indice = int(input('Escolha o índice para apagar: '))
-        lista_de_compras.pop(indice)
-        print(lista_de_compras)
-        continue
+        os.system('cls')
+        indice = input('Escolha o índice para apagar: ')
+        
+        try:
+            indice = int(indice)
+            del lista_de_compras[indice]
+        except ValueError:
+            print('Por favor digite um número inteiro')
+        except IndexError:
+            print('Índice não existe na lista')
 
     elif opcoes == 'l':
+        os.system('cls')
+
+        if len(lista_de_compras) == 0:
+            print('Nada para listar') 
+
         for indice, produto in enumerate(lista_de_compras):
                 print(f'{indice}', f' {produto}')
-
-    if opcoes == 'e':
-        print('Programa encerrado')
-        break  
