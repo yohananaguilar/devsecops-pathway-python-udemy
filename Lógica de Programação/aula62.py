e6658cc2f6cd66e7e88a920a46ad04412f6ed33f
y0shyy <yochagamer1@gmail.com>
2026-03-04 22:46:37 -0300

    Implementa cálculo do segundo dígito e cria gerador de CPF válido
    Conclui a lógica matemática do algoritmo de CPF e evolui o script para um gerador automático:
    - Implementa o cálculo do segundo dígito verificador utilizando a base de 10 dígitos e contagem regressiva a partir de 11.
    - Importa o módulo built-in 'random' para gerar dinamicamente os 9 primeiros dígitos via 'random.randint()'.
    - Concatena os dígitos aleatórios com os verificadores calculados para compor e exibir um CPF 100% válido matematicamente.

--- Lógica de Programação/aula62.py
+++ Lógica de Programação/aula62.py
@@ -24,10 +24,13 @@ contrário disso:
 
 O segundo dígito do CPF é 0
 """
+import random
 
+nove_digitos = ''
 
-cpf_enviado_usuario = '74682489070'
-nove_digitos = cpf_enviado_usuario[:9]
+for i in range(0, 9):
+    nove_digitos += str(random.randint(0, 9))
+
 contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
@@ -38,7 +41,6 @@ for digito in nove_digitos:
 digito_1 = (resultado_digito_1 * 10) % 11
 
 digito_1 = digito_1 if digito_1 <= 9 else 0
-
 
 dez_digitos = nove_digitos + str(digito_1)
 contador_regressivo_2 = 11
@@ -53,7 +55,4 @@ digito_2 = digito_2 if digito_2 <= 9 else 0
 
 cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
 
-if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
-    print(f'{cpf_enviado_usuario} é valido')
-else:
-    print(f'CPF inválido')
+print(cpf_gerado_pelo_calculo)
