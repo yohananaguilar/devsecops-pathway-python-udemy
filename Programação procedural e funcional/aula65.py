09ee8a3a3d2a77aace8dd16671bf97e4956d6799
y0shyy <yochagamer1@gmail.com>
2026-03-06 17:59:25 -0300

    Demonstra hierarquia de escopo global e local
    Explora a visibilidade e o ciclo de vida de variáveis no Python (Call Stack):
    - Exemplifica na prática a diferença conceitual entre escopo global e escopo local.
    - Demonstra o comportamento de sombreamento (shadowing) quando variáveis possuem o mesmo nome, mas existem em escopos diferentes (Call Space).
    - Utiliza funções aninhadas (nested functions) para ilustrar o isolamento de dados.

---
+++ Programação procedural e funcional/aula65.py
@@ -0,0 +1,24 @@
+"""
+Escopo de funções em Python
+Escopo significa o local onde aquele código pode atingir.
+Existe o escopo global e local.
+O escopo global é o escopo onde todo o código é alcançavel.
+O escopo local é o escopo onde apenas nomes do mesmo local
+podem ser alcançados.
+"""
+
+x = 1 
+
+def escopo():
+    x = 2
+
+    def interno_escopo():
+        x = 3
+        print(x)
+
+    interno_escopo()
+
+    print(x)
+
+escopo()
+print(x)
