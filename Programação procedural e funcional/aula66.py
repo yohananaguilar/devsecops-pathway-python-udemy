50e110da14834a6c3c057e9b714b0573be7ef701
y0shyy <yochagamer1@gmail.com>
2026-03-09 21:22:00 -0300

    Implementa retorno de valores em funções utilizando return
    Adiciona exemplos práticos do uso da palavra-chave 'return' em Python:
    - Demonstra como extrair o resultado do escopo local de uma função e armazená-lo em variáveis.
    - Aplica o conceito de 'early return' (retorno antecipado) utilizando blocos condicionais para interromper a execução da função.
    - Exemplifica a tipagem dinâmica do Python, retornando diferentes estruturas de dados (listas ou inteiros) a partir da mesma função dependendo da lógica de negócio.

---
+++ Programação procedural e funcional/aula66.py
@@ -0,0 +1,14 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+def soma(x, y):
+    if x > 10:
+        return[10, 20]
+    return x + y
+
+soma1 = soma(1, 1)
+soma2 = soma(2, 2)
+print(soma1)
+print(soma2)
+print(soma(11, 55))
