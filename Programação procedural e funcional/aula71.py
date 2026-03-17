6f534e43c4fcf08c094871daa185c8c01e6b268b
y0shyy <yochagamer1@gmail.com>
2026-03-17 17:49:48 -0300

    Introduz conceito de closures e funções fábrica (factory)
    Implementa o padrão de closure para retenção de estado na memória:
    - Cria a função 'criar_saudacao' para atuar como uma fábrica de funções especializadas.
    - Demonstra a preservação do escopo léxico, onde a função interna 'saudar' recorda o valor da variável original.
    - Utiliza um laço 'for' para instanciar e executar as funções geradas dinamicamente.

---
+++ Programação procedural e funcional/aula71.py
@@ -0,0 +1,15 @@
+"""
+Closure e funções que retornam outras funções
+"""
+
+def criar_saudacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}, {nome}'
+    return saudar
+
+falar_bom_dia = criar_saudacao('Bom Dia')
+falar_boa_noite = criar_saudacao('Boa Noite')
+
+for nome in ['Maria', 'Guilherme', 'João']:
+    print(falar_bom_dia(nome))
+    print(falar_boa_noite(nome))
